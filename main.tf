be30a3b001990db8196cfad9d4955828c896b3d3
Shea Phillips <shea.phillips@gmail.com>
2020-08-18 13:09:51 -0700

    Initial commit.

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
-//put your awesome Terraform code here.
+provider "keycloak" {
+	client_id = var.terraform_auth_client_id
+	client_secret = var.terraform_auth_client_secret
+	realm = var.realm
+	url = var.keycloak_base_url
+}
+
+locals {
+	iam_arn_prefix = "arn:aws:iam"
+	saml_provider = "saml-provider"
+	role = "role"
+
+	role_account_permutations = [for role_account in setproduct(var.accounts, var.role_names) :  {
+		account_number = role_account[0].account_number
+		role_name = role_account[1]
+		project_identifier = role_account[0].project_identifier
+		project_name = role_account[0].project_name
+		name = role_account[0].name
+		environment = role_account[0].environment
+	}
+	]
+}
+
+
+resource "keycloak_role" "roles" {
+	for_each = {
+	for role_account in local.role_account_permutations : "${role_account.account_number}/${role_account.role_name}" => role_account
+	}
+	realm_id = var.realm
+	client_id = var.iam_auth_client_id
+	name = "${local.iam_arn_prefix}::${each.value.account_number}:${local.saml_provider}/${var.idp_name},${local.iam_arn_prefix}::${each.value.account_number}:${local.role}/${each.value.role_name}"
+	description = "'${each.value.role_name}' role for account '${each.value.account_number}', the '${each.value.environment}' environment for project '${each.value.project_name}' with identifier '${each.value.project_identifier}'"
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,48 @@
-//module variables should be defined and documented here.
+variable "realm" {
+	description = "KeyCloak realm where terraform client has been created and where users/groups to be created/manipulated exist."
+	type        = string
+}
+
+variable "terraform_auth_client_id" {
+	description = "Client ID of client that terraform will authenticate against in order to do its work."
+	type        = string
+}
+
+variable "terraform_auth_client_secret" {
+	description = "Client secret used by Terraform KeyCloak provider authenticate against KeyCloak."
+	type = string
+}
+
+variable "iam_auth_client_id" {
+	description = "Client ID of client where IAM roles will be created."
+	type        = string
+}
+
+variable "keycloak_base_url" {
+	description = "Base URL of KeyCloak instance to interact with."
+	type = string
+}
+
+variable "accounts" {
+	description = "List of accounts that product teams' workloads run within."
+	type = list(object({
+		project_identifier = string
+		project_name = string
+		name = string
+		environment = string
+		account_number = string
+	}))
+}
+
+//IAM roles names
+variable "role_names" {
+	description = "A list of the role names in the IAM system.  These will be used along with other elelments to construct the KC role names."
+	type = list(string)
+}
+
+variable "idp_name" {
+	description = "Name of configured IDP in AWS."
+	type = string
+	default = "BCGovKeyCloak"
+}
+
