37623ec323482312523d337f5f7d11f2c53481d5
Shea Phillips <shea.phillips@gmail.com>
2021-03-09 20:30:46 -0800

    fix error in massaging of output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "roles" {
 	value = [ for role in keycloak_role.roles : merge(role, {
-		account_number = split(":", split(",", role.name))[2]
+		account_number = split(":", split(",", role.name)[0])[2]
 	}) ]
 }
