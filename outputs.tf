0e81ab18eddbe1d3efbdcc298b5f4780f1730808
Shea Phillips <shea.phillips@gmail.com>
2021-03-10 21:24:39 -0800

    cleanup

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,6 @@
 output "roles" {
 	value = [ for role in keycloak_role.roles : merge(role, {
 		account_number = split(":", split(",", role.name)[0])[4],
-//		${local.iam_arn_prefix}::${each.value.account_number}:${local.role}/${each.value.role_name}
 		aws_role_name = split("/", split(":", split(",", role.name)[1])[5])[1],
 	}) ]
 }
