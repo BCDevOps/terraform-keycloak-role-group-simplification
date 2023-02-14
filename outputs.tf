36b086f54d9168e0d77581ae7674e5e7a343bd45
Warren Uniewski <warren@autimo.com>
2023-02-13 16:30:12 -0800

    add gha workflows

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "roles" {
-	value = [ for role in keycloak_role.roles : merge(role, {
-		account_number = split(":", split(",", role.name)[0])[4],
-		aws_role_name = split("/", split(":", split(",", role.name)[1])[5])[1],
-	}) ]
+  value = [for role in keycloak_role.roles : merge(role, {
+    account_number = split(":", split(",", role.name)[0])[4],
+    aws_role_name  = split("/", split(":", split(",", role.name)[1])[5])[1],
+  })]
 }
--- variables.tf
+++ variables.tf
@@ -1,33 +1,33 @@
 variable "realm" {
-	description = "KeyCloak realm where terraform client has been created and where users/groups to be created/manipulated exist."
-	type        = string
+  description = "KeyCloak realm where terraform client has been created and where users/groups to be created/manipulated exist."
+  type        = string
 }
 
 variable "iam_auth_client_id" {
-	description = "Client ID of client where IAM roles will be created."
-	type        = string
+  description = "Client ID of client where IAM roles will be created."
+  type        = string
 }
 
 variable "accounts" {
-	description = "List of accounts that product teams' workloads run within."
-	type = list(object({
-		project_identifier = string
-		project_name = string
-		name = string
-		environment = string
-		account_number = string
-	}))
+  description = "List of accounts that product teams' workloads run within."
+  type = list(object({
+    project_identifier = string
+    project_name       = string
+    name               = string
+    environment        = string
+    account_number     = string
+  }))
 }
 
 //IAM roles names
 variable "role_names" {
-	description = "A list of the role names in the IAM system.  These will be used along with other elelments to construct the KC role names."
-	type = list(string)
+  description = "A list of the role names in the IAM system.  These will be used along with other elelments to construct the KC role names."
+  type        = list(string)
 }
 
 variable "idp_name" {
-	description = "Name of configured IDP in AWS."
-	type = string
-	default = "BCGovKeyCloak"
+  description = "Name of configured IDP in AWS."
+  type        = string
+  default     = "BCGovKeyCloak"
 }
 
